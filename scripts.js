7298910b4d4351cbea2c3f8827431247151d0beb
DjonataBorba <djonata16@hotmail.com>
2025-03-08 16:00:39 -0300

    deixei o site responsivo, fazendo as alterações com @media

--- scripts.js
+++ scripts.js
@@ -39,3 +39,19 @@ prevButton.onclick = () => {
 
     indicator.querySelector('.number').innerText = '0' + (active + 1)
 }
+
+let btnmenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnmenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu') 
+})
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu') 
+})
